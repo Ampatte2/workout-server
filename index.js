a18b1c9602d76353c7ba6c15216b8056caa7efd0
Ampatte2 <Ampatte2@asu.edu>
2019-12-09 16:58:39 -0700

    Access-Control-Allow-Origin *

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", (req,res)=>{
 });
 
 app.use("/api", workoutRouter, (req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", ["*"]);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type")
 });
