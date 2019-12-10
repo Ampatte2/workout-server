205f9fbedb6bf690ec95cb539243ca8bacb134f1
Ampatte2 <Ampatte2@asu.edu>
2019-12-09 17:05:01 -0700

    Removed Cors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+
 const app = express();
 const apiPort = 3000;
 const db = require("./DB");
 const workoutRouter = require("./routes/workout-router");
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors());
+
 app.use(bodyParser.json())
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
