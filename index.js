6cac3e47c5779245ff0cd6b4a1f3b78c624a3ceb
Ampatte2 <Ampatte2@asu.edu>
2019-12-10 23:03:48 -0700

    production

--- index.js
+++ index.js
@@ -25,5 +25,10 @@ app.use("/api", workoutRouter, (req, res, next)=>{
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type")
 });
-
+if(process.env.NODE_ENV=== "production"){
+    app.use(express.static("client/build"))
+}
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
 app.listen(apiPort, ()=> console.log(`Server running on port ${apiPort}`));
