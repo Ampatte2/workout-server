178830a55ab32a10c656ee3ee0c7dbb3a278adb4
Ampatte2 <Ampatte2@asu.edu>
2019-12-10 23:07:48 -0700

    new way

--- index.js
+++ index.js
@@ -11,24 +11,23 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(bodyParser.json())
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"))
-
-
-
-app.get("/", (req,res)=>{
-    res.send(JSON.stringify({Hello:"World"}))
-    console.log("Hello")
-});
-
-app.use("/api", workoutRouter, (req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type")
-});
 if(process.env.NODE_ENV=== "production"){
     app.use(express.static("client/build"))
 }
 app.get("*", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 })
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"))
+
+
+
+
+
+app.use("/api", workoutRouter, (req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type")
+});
+
 app.listen(apiPort, ()=> console.log(`Server running on port ${apiPort}`));
